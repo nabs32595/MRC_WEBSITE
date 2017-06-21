5647b451dfd0e5ae3e66192aecc0e2170b136ab8
nabil <nabil.k.alkaff@gmail.com>
2017-06-21 22:33:55 +0800

    i deleted

--- addition.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$num1 = 4; $num2 = 5;
-
-$total = $num1 + $num2;
-
-echo "the total is". $total; 
-
-?>
---
+++ admin.php
@@ -0,0 +1,9 @@
+<?php
+
+$num1 = 3; $num2= 4; 
+
+$total = $num1 + $num2;
+
+echo $total;
+
+?>
